e4b999bdbbfa47567e380cba3829d896d932af5a
Ivan <Ivan@Grisel-PC>
2012-06-12 22:58:22 -0300

    Ultima version

---
+++ proyecto-choques/src/controlp5/ejemplos/ControlP5Test.java
@@ -0,0 +1,17 @@
+package controlp5.ejemplos;
+
+import processing.core.PApplet;
+import controlP5.ControlP5;
+
+public class ControlP5Test extends ControlP5 {
+
+	public ControlP5Test(PApplet theParent) {
+		super(theParent);
+	}
+	
+	@Override
+	public void pre(){
+		//this.
+	}
+
+}
